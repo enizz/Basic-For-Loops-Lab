d2e48d11c18d331ac9739bffe5338f623ead3e3d
Eugene <Eugene.Nisnevich@Gmail.com>
2022-07-06 11:05:56 -0400

    Add project files.

---
+++ Basic For-Loops Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿ do
+    {
+        Console.WriteLine("Enter a number..");
+
+        int x = int.Parse(Console.ReadLine());
+        int y = 0;
+
+        for (int i = 1; i <= x; i++)
+        {
+            y += i;
+        }
+
+        Console.WriteLine("The sum of all the numbers from 1 to " + x + " is: " + y);
+        Console.WriteLine("Press enter to try again..");
+    }
+while (Console.ReadKey().Key == ConsoleKey.Enter);
